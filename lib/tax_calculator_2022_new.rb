68b35fabd8900951589ffeecbcef02ab63865b72
Ikram Javad <ikram.javad@xprolabs.com>
2022-07-18 19:27:33 +0500

    first commit

---
+++ lib/tax_calculator_2022_new.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tax_calculator_2022_new/version"
+
+module TaxCalculator2022New
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tax_calculator_2022_new/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TaxCalculator2022New
+  VERSION = "0.1.0"
+end
